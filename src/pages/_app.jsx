b466e5648f8f9c193ed406b7739f4d4ff75c8381
Sanchitha Dinesh <sanchunila@gmail.com>
2023-05-11 21:13:14 -0700

    Update hack title

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -32,7 +32,7 @@ export default function MyApp({Component, pageProps}) {
   return (
     <div>
       <Head>
-        <title>Supernova Lynbroook Hackathon</title>
+        <title>Supernova Hackathon</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
